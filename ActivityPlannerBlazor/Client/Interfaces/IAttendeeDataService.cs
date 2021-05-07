0dbb2cfaa0943a2a95b0872c74e73c605a91978c
hiwad rashad <hiwadrashad112@hotmail.com>
2021-05-07 11:26:39 +0200

    Interfaces added

---
+++ ActivityPlannerBlazor/Client/Interfaces/IAttendeeDataService.cs
@@ -0,0 +1,17 @@
+﻿using ActivityPlannerBlazor.Shared.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivityPlannerBlazor.Client.Interfaces
+{
+    public interface IAttendeeDataService
+    {
+        Task<IEnumerable<AttendeeDTO>> GetAll();
+        Task<AttendeeDTO> GetDetails(string id);
+        Task<AttendeeDTO> Add(AttendeeDTO model);
+        Task Update(AttendeeDTO model);
+        Task Delete(string id);
+    }
+}
---
+++ ActivityPlannerBlazor/Client/Interfaces/IOrganizerDataService.cs
@@ -0,0 +1,17 @@
+﻿using ActivityPlannerBlazor.Shared.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivityPlannerBlazor.Client.Interfaces
+{
+    public interface IOrganizerDataService
+    {
+        Task<IEnumerable<OrganizerDTO>> GetAll();
+        Task<OrganizerDTO> GetDetails(string id);
+        Task<OrganizerDTO> Add(OrganizerDTO model);
+        Task Update(OrganizerDTO model);
+        Task Delete(string id);
+    }
+}
