b3838a065bdf8273bfd13e9a75af8183dc2d99c1
hiwad rashad <hiwadrashad112@hotmail.com>
2021-05-11 16:00:18 +0200

    Added Overview Page

--- ActivityPlannerBlazor/Client/Pages/AppointmentOverview.cs
+++ ActivityPlannerBlazor/Client/Pages/AppointmentOverview.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ActivityPlannerBlazor.Client.Interfaces;
+using ActivityPlannerBlazor.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,20 @@ namespace ActivityPlannerBlazor.Client.Pages
 {
     public partial class AppointmentOverview
     {
+        [Inject]
+        public ICurrentOrganizerDataService CurrentOrganizerDataService { get; set; }
+
+        [Inject]
+        public IOrganizerDataService OrganizerDataService { get; set; }
+
+        [Inject]
+        public IAttendeeDataService AttendeeDataService { get; set; }
+
+        public OrganizerDTO CurrentOrganizer { get; set; } = new OrganizerDTO() { };
+
+        protected override async Task OnInitializedAsync()
+        {
+            CurrentOrganizer = await CurrentOrganizerDataService.GetCurrentUser();
+        }
     }
 }
