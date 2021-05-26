a4626a5f2b43f6e275108ded9aa4f193c35c1292
hiwad rashad <hiwadrashad112@hotmail.com>
2021-05-26 15:15:30 +0200

    Unit Testing added

---
+++ ActivityPlannerBlazor/Client/Pages/SingleButtonTestCodePage.cs
@@ -0,0 +1,17 @@
+﻿using ActivityPlannerBlazor.Shared.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivityPlannerBlazor.Client.Pages
+{
+    public partial class SingleButtonTestCodePage
+    {
+        AttendeeDTO CurrentAttendee = new AttendeeDTO() { };
+
+        public void ExecuteCode()
+        { 
+        }
+    }
+}
