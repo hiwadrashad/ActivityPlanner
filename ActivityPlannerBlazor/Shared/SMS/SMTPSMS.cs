20a0f05a82654643674295e3029ebcc85db603ff
hiwad rashad <hiwadrashad112@hotmail.com>
2021-05-26 15:37:05 +0200

    added blackbox whitebox testing

--- ActivityPlannerBlazor/Shared/SMS/SMTPSMS.cs
+++ ActivityPlannerBlazor/Shared/SMS/SMTPSMS.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿using ActivityPlannerBlazor.Shared.DTOS;
+using ActivityPlannerBlazor.Shared.Messages;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 
 namespace ActivityPlannerBlazor.Shared.SMS
 {
     public class SMTPSMS
     {
-        public static void SendSMS()
+        public static void SendSMS(AttendeeDTO attendee, AppointmentDTO appointment, OrganizerDTO organizer)
         {
-           
+            SmtpClient smtp = new SmtpClient();
+            MailMessage message = new MailMessage();
+            smtp.Credentials = new System.Net.NetworkCredential("testmailopdracht@gmail.com", "testmail");
+            smtp.Host = "ipipi.com";
+            message.From = new MailAddress("testmailopdracht@gmail.com");
+            message.To.Add(attendee.Data.Email);
+            message.Subject = "activity planner invite";
+            message.Body = MessageGenerator.ReturnMessage(organizer, appointment);
+            smtp.Send(message);
         }
     }
 }
