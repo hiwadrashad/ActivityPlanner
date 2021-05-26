58093417ca93d9738feeae500f42872585fe744a
hiwad rashad <hiwadrashad112@hotmail.com>
2021-05-26 14:56:43 +0200

    branching update

---
+++ ActivityPlannerBlazor/Shared/SMS/SMTPSMS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityPlannerBlazor.Shared.SMS
+{
+    public class SMTPSMS
+    {
+        public static void SendSMS()
+        {
+           
+        }
+    }
+}
